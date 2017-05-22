dd9f2a5a85fed785b9a3aaf7d56f4bee70d35e71
knies <knies@10.0.1.47>
2017-05-21 21:51:49 -0400

    Done

--- RocketDAL/src/test/java/rocketBase/Rate_Test.java
+++ RocketDAL/src/test/java/rocketBase/Rate_Test.java
@@ -10,13 +10,17 @@ import rocketDomain.RateDomainModel;
 
 public class Rate_Test {
 
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
+	@Test
+	public void test1(){
+		ArrayList<RateDomainModel> rts = RateDAL.getAllRates();
+		assert(rts.size() > 0);
+		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
+		assertEquals(rates.get(0).getdInterestRate(),7.00,0.01);
+		assertEquals(rates.get(1).getdInterestRate(),6.00,0.01);
+		assertEquals(rates.get(2).getdInterestRate(),6.25,0.01);
+		assertEquals(rates.get(3).getdInterestRate(),5.25,0.01);
+		assertEquals(rates.get(4).getdInterestRate(),5.50,0.01);
 
 
+	}
 }
